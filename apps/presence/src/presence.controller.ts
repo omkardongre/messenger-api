d375c0a538af59f2a534c13a43589d2970e0c2f6
omkardongre <44136758+omkardongre@users.noreply.github.com>
2024-10-31 02:53:53 +0530

    test(presence): add dummy endpoint to verify RMQ connection

--- apps/presence/src/presence.controller.ts
+++ apps/presence/src/presence.controller.ts
@@ -1,13 +1,24 @@
 import { Controller, Get } from '@nestjs/common';
 import { PresenceService } from './presence.service';
 import { SharedService } from '@app/shared';
+import { Ctx } from '@nestjs/microservices';
+import { MessagePattern, RmqContext } from '@nestjs/microservices';
 
 @Controller()
 export class PresenceController {
-  constructor(private readonly presenceService: PresenceService) {}
+  constructor(
+    private readonly presenceService: PresenceService,
+    private readonly sharedService: SharedService,
+  ) {}
 
   @Get()
   getHello(): string {
+    return this.presenceService.getHello();
+  }
+
+  @MessagePattern({ cmd: 'get-presence' })
+  getPresence(@Ctx() context: RmqContext) {
+    this.sharedService.acknowledgeMessage(context);
     return this.presenceService.getHello();
   }
 }
