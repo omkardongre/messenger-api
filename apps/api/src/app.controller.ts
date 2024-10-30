ef2f4f16df87bdbf766bea0b919263ee86addec2
omkardongre <44136758+omkardongre@users.noreply.github.com>
2024-10-30 23:43:13 +0530

    refactor(api): improve service naming in app controller
    - Rename authService to authClientProxy for better clarity

--- apps/api/src/app.controller.ts
+++ apps/api/src/app.controller.ts
@@ -3,15 +3,15 @@ import { ClientProxy } from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
-  constructor(@Inject('AUTH_SERVICE') private authService: ClientProxy) {}
+  constructor(@Inject('AUTH_SERVICE') private authClientProxy: ClientProxy) {}
 
   @Get('auth')
   async getUsers() {
-    return this.authService.send({ cmd: 'get-users' }, {});
+    return this.authClientProxy.send({ cmd: 'get-users' }, {});
   }
 
   @Post('auth')
   async postUser() {
-    return this.authService.send({ cmd: 'post-user' }, {});
+    return this.authClientProxy.send({ cmd: 'post-user' }, {});
   }
 }
