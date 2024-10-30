c8f6c7efa5ffe06ebb440d182e7965dd90b3bedb
omkardongre <44136758+omkardongre@users.noreply.github.com>
2024-10-30 23:46:20 +0530

    refactor(microservices): centralize RabbitMQ connection setup
    - Move RabbitMQ client setup from app.module to shared.module
    - Add dynamic module method registerRmq() for queue configuration
    - Configure auth and presence service queues using shared module

--- libs/shared/src/shared.module.ts
+++ libs/shared/src/shared.module.ts
@@ -1,6 +1,8 @@
-import { Module } from '@nestjs/common';
+import { DynamicModule, Module } from '@nestjs/common';
 import { SharedService } from './shared.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ClientProxy, Transport } from '@nestjs/microservices';
+import { ClientProxyFactory } from '@nestjs/microservices';
 
 @Module({
   imports: [
@@ -12,4 +14,33 @@ import { ConfigModule } from '@nestjs/config';
   providers: [SharedService],
   exports: [SharedService],
 })
-export class SharedModule {}
+export class SharedModule {
+  static registerRmq(service: string, queue: string): DynamicModule {
+    const serviceProvider = {
+      provide: service,
+      useFactory: (configService: ConfigService): ClientProxy => {
+        const USER = configService.get<string>('RABBITMQ_USER');
+        const PASSWORD = configService.get<string>('RABBITMQ_PASSWORD');
+        const HOST = configService.get<string>('RABBITMQ_HOST');
+
+        return ClientProxyFactory.create({
+          transport: Transport.RMQ,
+          options: {
+            urls: [`amqp://${USER}:${PASSWORD}@${HOST}`],
+            queue: queue,
+            queueOptions: {
+              durable: true,
+            },
+          },
+        });
+      },
+      inject: [ConfigService],
+    };
+
+    return {
+      module: SharedModule,
+      providers: [serviceProvider],
+      exports: [serviceProvider],
+    };
+  }
+}
